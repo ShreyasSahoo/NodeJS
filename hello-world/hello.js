09162cf898a4c03809c0ac038a103a6574763d75
Shreyas Sahoo <shreyassahoo26@gmail.com>
2023-10-20 19:47:42 +0530

    basics of nodejs

---
+++ hello-world/hello.js
@@ -0,0 +1,3 @@
+//JS was made to run in browsers. Every browser has an engine of its own to run js, like chrome has javascript v8 engine.
+//A scientist "Ryan Dahl" extracted out this v8 engine , embedded it with c++ so that js can be run outside the browser as well. In this process, javascript lost some of its dom & window features and gained some low level features like file handling.
+console.log("Hey there! I am JavaScript");
